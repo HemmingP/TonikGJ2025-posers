6fca4e7cc462efe1220db3c2d4d045b0dffa13c1
flowcore-platform <gitops@flowcore.io>
2025-05-03 16:36:54 +0100

    Added joint validation to round manager, and setup a demo session

--- posers/Assets/Scripts/Management/RoundManager.cs
+++ posers/Assets/Scripts/Management/RoundManager.cs
@@ -9,27 +9,13 @@ public class RoundManager : MonoBehaviour
     [SerializeField] private bool startOnStart = false;
     [SerializeField] private StartTimer timer;
 
+    [SerializeField] private JointCollector[] jointCollectors;
 
     private List<JointValidator> availablePoses = new List<JointValidator>();
-
     private JointValidator currentPose;
 
     private void Start() {
-        foreach(var pose in poses) {
-            var newPose = Instantiate(pose.gameObject);
-            
-            newPose.transform.SetParent(spawnLocation);
-            newPose.transform.localPosition = Vector3.zero;
-            newPose.transform.localRotation = Quaternion.identity;
-            newPose.transform.localScale = Vector3.one;
-
-            newPose.SetActive(false);
-
-            var validator = newPose.GetComponent<JointValidator>();
-            Assert.IsTrue(validator != null, "Failed to create pose, because the prefab is missing the JointValidator component");
-            
-            availablePoses.Add(validator);
-        }
+        GenerateAvailablePoses();
 
         if (startOnStart) {
             StartRound();
@@ -46,5 +32,35 @@ public class RoundManager : MonoBehaviour
         currentPose.gameObject.SetActive(true);
         
         timer.TriggerTimer();
+    }
+
+    public void ValidateRound() {
+        Assert.IsTrue(currentPose != null, "Failed to validate round, because no pose is currently active. This should never happen!");
+
+        foreach(var collector in jointCollectors) {
+            var recordedJoints = collector.recordedJoints;
+            var result = currentPose.CompareAccuracy(recordedJoints);
+            Debug.Log($"Round Result: {result.accuracy}%");
+        }
+
+        StartRound();
+    }
+
+    private void GenerateAvailablePoses() {
+        foreach(var pose in poses) {
+            var newPose = Instantiate(pose.gameObject);
+            
+            newPose.transform.SetParent(spawnLocation);
+            newPose.transform.localPosition = Vector3.zero;
+            newPose.transform.localRotation = Quaternion.identity;
+            newPose.transform.localScale = Vector3.one;
+
+            newPose.SetActive(false);
+
+            var validator = newPose.GetComponent<JointValidator>();
+            Assert.IsTrue(validator != null, "Failed to create pose, because the prefab is missing the JointValidator component");
+            
+            availablePoses.Add(validator);
+        }
     }
 }
