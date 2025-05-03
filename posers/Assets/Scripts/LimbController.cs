b4f864e4faf7d88f5e7f2f9910d87fd2358d81c7
HemmingP <hemming_lol@hotmail.com>
2025-05-03 16:56:39 +0100

    Character control

--- posers/Assets/Scripts/LimbController.cs
+++ posers/Assets/Scripts/LimbController.cs
@@ -1,9 +1,11 @@
+using System.Threading;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent (typeof(PlayerInput))]
 public class LimbController : MonoBehaviour
 {
+    public float motorSpeed = 100;
     public HingeJoint2D leftShoulder;
     public HingeJoint2D leftElbow;
     public HingeJoint2D rightShoulder;
@@ -12,18 +14,46 @@ public class LimbController : MonoBehaviour
     public HingeJoint2D leftKnee;
     public HingeJoint2D rightHip;
     public HingeJoint2D rightKnee;
+    private HingeJoint2D[] hingeJoints;
 
     private bool isInversed = false;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        JointMotor2D a = new JointMotor2D();
-        a.motorSpeed = 123;
+        hingeJoints = new HingeJoint2D[] {leftShoulder, leftElbow, rightShoulder, rightElbow, leftHip, leftKnee, rightHip, rightKnee};
     }
 
     // Update is called once per frame
     void Update()
     {
+        foreach(HingeJoint2D hingeJoint2D in hingeJoints)
+        {
+            MonitorJoint(hingeJoint2D);
+        }
+    }
+
+    private void MonitorJoint(HingeJoint2D joint)
+    {
+        if (!joint.useMotor)
+            return;
+
+        JointMotor2D motor = joint.motor;
+
+        // If there's a target speed but joint is not rotating
+        if (Mathf.Abs(motor.motorSpeed) > 0.01f && Mathf.Abs(joint.jointSpeed) < 0.1f)
+        {
+            float angle = joint.jointAngle;
+            JointAngleLimits2D limits = joint.limits;
+
+            bool atMin = angle <= limits.min + 1;
+            bool atMax = angle >= limits.max - 1;
+
+            if ((motor.motorSpeed < 0 && atMin) || (motor.motorSpeed > 0 && atMax))
+            {
+                // Disable motor if stuck at limit
+                joint.useMotor = false;
+            }
+        }
     }
 
     public void OnInverseDirection(InputValue value)
@@ -57,46 +87,47 @@ public class LimbController : MonoBehaviour
     public void OnMoveLeftShoulder(InputValue value)
     {
         bool pressed = value.isPressed;
-        MoveLimb(leftShoulder, (pressed ? -100 : 0) * (isInversed ? -1 : 1));
+        MoveLimb(leftShoulder, (pressed ? -motorSpeed : 0) * (isInversed ? -1 : 1));
     }
     public void OnMoveLeftElbow(InputValue value)
     {
         bool pressed = value.isPressed;
-        MoveLimb(leftElbow, (pressed ? 100 : 0) * (isInversed ? -1 : 1));
+        MoveLimb(leftElbow, (pressed ? motorSpeed : 0) * (isInversed ? -1 : 1));
     }
     public void OnMoveRightShoulder(InputValue value)
     {
         bool pressed = value.isPressed;
-        MoveLimb(rightShoulder, (pressed ? 100 : 0) * (isInversed ? -1 : 1));
+        MoveLimb(rightShoulder, (pressed ? motorSpeed : 0) * (isInversed ? -1 : 1));
     }
     public void OnMoveRightElbow(InputValue value)
     {
         bool pressed = value.isPressed;
-        MoveLimb(rightElbow, (pressed ? -100 : 0) * (isInversed ? -1 : 1));
+        MoveLimb(rightElbow, (pressed ? -motorSpeed : 0) * (isInversed ? -1 : 1));
     }
     public void OnMoveLeftHip(InputValue value)
     {
         bool pressed = value.isPressed;
-        MoveLimb(leftHip, (pressed ? -100 : 0) * (isInversed ? -1 : 1));
+        MoveLimb(leftHip, (pressed ? -motorSpeed : 0) * (isInversed ? -1 : 1));
     }
     public void OnMoveLeftKnee(InputValue value)
     {
         bool pressed = value.isPressed;
-        MoveLimb(leftKnee, (pressed ? -100 : 0) * (isInversed ? -1 : 1));
+        MoveLimb(leftKnee, (pressed ? -motorSpeed : 0) * (isInversed ? -1 : 1));
     }
     public void OnMoveRightHip(InputValue value)
     {
         bool pressed = value.isPressed;
-        MoveLimb(rightHip, (pressed ? 100 : 0) * (isInversed ? -1 : 1));
+        MoveLimb(rightHip, (pressed ? motorSpeed : 0) * (isInversed ? -1 : 1));
     }
     public void OnMoveRightKnee(InputValue value)
     {
         bool pressed = value.isPressed;
-        MoveLimb(rightKnee, (pressed ? 100 : 0) * (isInversed ? -1 : 1));
+        MoveLimb(rightKnee, (pressed ? motorSpeed : 0) * (isInversed ? -1 : 1));
     }
 
-    private void MoveLimb(HingeJoint2D whatLimb, float motorSpeed)
-    {
-        whatLimb.motor = new JointMotor2D { motorSpeed = motorSpeed, maxMotorTorque = 10000 };
-    }
+private void MoveLimb(HingeJoint2D joint, float speed)
+{
+    joint.useMotor = speed != 0;
+    joint.motor = new JointMotor2D { motorSpeed = speed, maxMotorTorque = 10000 };
+}
 }
