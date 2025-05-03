3e3d3f2ab8297951b29bd27b6d24a79b3b05c59d
flowcore-platform <gitops@flowcore.io>
2025-05-03 14:42:03 +0100

    added visuals and neighbouring parent calculation in joint record

--- posers/Assets/Scripts/Posing/JointRecord.cs
+++ posers/Assets/Scripts/Posing/JointRecord.cs
@@ -4,11 +4,31 @@ public class JointRecord : MonoBehaviour
 {
   public ScriptableEnum joint;
   
+  [Tooltip("If there is a parent, but it is a neighbouring object, instead of a direct transform parent")]
+  public GameObject manualParent;
+  
   private void OnDrawGizmos()
   {
     Gizmos.color = Color.red;
     Gizmos.DrawWireSphere(transform.position, 0.1f);
   }
 
-  public float angle => transform.localRotation.eulerAngles.z;
+  public float angle {
+    get {
+      if (manualParent == null) {
+        return transform.localRotation.eulerAngles.z;
+      }
+
+      // convert the rotation of this object to be calculated as if it was the child of the manual parent object
+      var parentMatrix = manualParent.transform.localToWorldMatrix;
+      var thisMatrix = transform.localToWorldMatrix;
+
+      var parentRotation = parentMatrix.rotation.eulerAngles.z;
+      var thisRotation = thisMatrix.rotation.eulerAngles.z;
+
+      return thisRotation - parentRotation;
+    }
+  }
+
+
 }
