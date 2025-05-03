f401c424cbd88535f8d9c460c6c7ee6184d3fa2f
flowcore-platform <gitops@flowcore.io>
2025-05-03 23:21:36 +0100

    added time reduction per round

--- posers/Assets/Scripts/Management/RoundManager.cs
+++ posers/Assets/Scripts/Management/RoundManager.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.Events;
 
-public struct RoundResult {
+public struct RoundResult
+{
     public JointValidationResult result;
     public string playerName;
 }
@@ -16,44 +17,53 @@ public class RoundManager : MonoBehaviour
     [SerializeField] private StartTimer timer;
 
     [SerializeField] private float minimumAccuracy = 80.0f;
+    [SerializeField] private int reductionInSecondsPerRound = 3;
     [SerializeField] private JointCollector[] jointCollectors;
-    
+
     [SerializeField] private UnityEvent<string> onGameEnded;
     [SerializeField] private UnityEvent onGameTied;
 
     private List<JointValidator> availablePoses = new List<JointValidator>();
     private JointValidator currentPose;
 
-    private void Start() {
+    private void Start()
+    {
         GenerateAvailablePoses();
 
-        if (startOnStart) {
+        if (startOnStart)
+        {
             StartRound();
         }
     }
 
-    public void StartRound() {
-        if(currentPose != null) {
+    public void StartRound()
+    {
+        if (currentPose != null)
+        {
             currentPose.gameObject.SetActive(false);
         }
 
         var selectedPose = availablePoses[Random.Range(0, availablePoses.Count)];
         currentPose = selectedPose;
         currentPose.gameObject.SetActive(true);
-        
+
         timer.TriggerTimer();
+        timer.ReduceTotalSeconds(reductionInSecondsPerRound);
     }
 
-    public void ValidateRound() {
+    public void ValidateRound()
+    {
         Assert.IsTrue(currentPose != null, "Failed to validate round, because no pose is currently active. This should never happen!");
 
         var collections = new List<RoundResult>();
 
-        foreach(var collector in jointCollectors) {
+        foreach (var collector in jointCollectors)
+        {
             var recordedJoints = collector.recordedJoints;
             var result = currentPose.CompareAccuracy(recordedJoints);
 
-            collections.Add(new RoundResult {
+            collections.Add(new RoundResult
+            {
                 result = result,
                 playerName = collector.playerName
             });
@@ -62,14 +72,19 @@ public class RoundManager : MonoBehaviour
         var accuracyBelowMinimum = collections.FindAll(result => result.result.accuracy < minimumAccuracy);
 
         // is the game a tie?
-        if(accuracyBelowMinimum.Count == jointCollectors.Length) {
+        if (accuracyBelowMinimum.Count == jointCollectors.Length)
+        {
             onGameTied?.Invoke();
             return;
         }
-        
+
         // was anyone below the minimum accuracy?
-        if(accuracyBelowMinimum.Count > 0) {
-            onGameEnded?.Invoke(accuracyBelowMinimum[0].playerName);
+        if (accuracyBelowMinimum.Count > 0)
+        {
+            var winningPlayer = collections.Find(result => result.result.accuracy >= minimumAccuracy);
+            var playerName = winningPlayer.playerName;
+
+            onGameEnded?.Invoke($"{playerName} Wins!");
             return;
         }
 
@@ -77,10 +92,12 @@ public class RoundManager : MonoBehaviour
         StartRound();
     }
 
-    private void GenerateAvailablePoses() {
-        foreach(var pose in poses) {
+    private void GenerateAvailablePoses()
+    {
+        foreach (var pose in poses)
+        {
             var newPose = Instantiate(pose.gameObject);
-            
+
             newPose.transform.SetParent(spawnLocation);
             newPose.transform.localPosition = Vector3.zero;
             newPose.transform.localRotation = Quaternion.identity;
@@ -90,7 +107,7 @@ public class RoundManager : MonoBehaviour
 
             var validator = newPose.GetComponent<JointValidator>();
             Assert.IsTrue(validator != null, "Failed to create pose, because the prefab is missing the JointValidator component");
-            
+
             availablePoses.Add(validator);
         }
     }
