2851c98e85db32317c619e0249b42e44aec266a6
flowcore-platform <gitops@flowcore.io>
2025-05-04 16:36:41 +0100

    added final music to main scene

--- posers/Assets/Scripts/Management/MusicManagement.cs
+++ posers/Assets/Scripts/Management/MusicManagement.cs
@@ -10,35 +10,41 @@ public class MusicManagement : MonoBehaviour
     [SerializeField] private AudioClip gameEnded;
 
     [SerializeField] private float delayBetweenPlays = 0.5f;
+    [SerializeField] private float roundedEndedDelay = 0.7f;
 
 
-    private void PlaySound(AudioClip clip)
+    private void Start()
+    {
+        StartCoroutine(WaitUntilSongStartsAgain(delayBetweenPlays));
+    }
+
+    private void PlaySound(AudioClip clip, float delay)
     {
         backgroundMusicSource.Stop();
 
         audioSource.clip = clip;
         audioSource.Play();
 
-        StopCoroutine(WaitUntilSongStartsAgain());
-        StartCoroutine(WaitUntilSongStartsAgain());
+        StopAllCoroutines();
+        StartCoroutine(WaitUntilSongStartsAgain(delay));
     }
 
     public void PlayRoundEnded()
     {
-        PlaySound(roundEnded);
+        PlaySound(roundEnded, roundedEndedDelay);
     }
 
 
     public void PlayGameEnded()
     {
-        PlaySound(gameEnded);
+        PlaySound(gameEnded, delayBetweenPlays);
     }
 
-    IEnumerator WaitUntilSongStartsAgain()
+    IEnumerator WaitUntilSongStartsAgain(float delay)
     {
         do
         {
-            yield return new WaitForSeconds(delayBetweenPlays);
+            yield return new WaitForSeconds(delay);
         } while (audioSource.isPlaying);
 
         backgroundMusicSource.Play();
