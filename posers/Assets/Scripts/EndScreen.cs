9abb93df387834a4cc277ebbe1af72442f651d82
HemmingP <hemming_lol@hotmail.com>
2025-05-04 14:28:53 +0100

    Win and loose sounds

--- posers/Assets/Scripts/EndScreen.cs
+++ posers/Assets/Scripts/EndScreen.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
+public enum WinStatus{
+    NO_ONE,
+    ANGELO,
+    FRAZ
+}
 
+[RequireComponent (typeof(AudioSource))]
 public class EndScreen : MonoBehaviour
 {
     public TextMeshProUGUI textWinner;
     public GameObject anyKeyGO;
+    [SerializeField] private AudioClip[] angeloWinsSounds;
+    [SerializeField] private AudioClip[] franzWinsSounds;
+    [SerializeField] private AudioClip[] bothLooseSounds;
+    private AudioSource audioSource => GetComponent<AudioSource>();
     public void Toggle()
     {
         GetComponent<Animator>().SetTrigger("Toggle Endscreen");
@@ -19,5 +29,22 @@ public class EndScreen : MonoBehaviour
     public void EnablePressAnyKey()
     {
         anyKeyGO.SetActive(true);
+    }
+
+    public void PlayEndSound(WinStatus winStatus)
+    {
+        if(winStatus == WinStatus.NO_ONE)
+        {
+            audioSource.resource = bothLooseSounds[Random.Range(0, bothLooseSounds.Length)];
+        }
+        if(winStatus == WinStatus.ANGELO)
+        {
+            audioSource.resource = angeloWinsSounds[Random.Range(0, angeloWinsSounds.Length)];
+        }
+        if(winStatus == WinStatus.FRAZ)
+        {
+            audioSource.resource = franzWinsSounds[Random.Range(0, franzWinsSounds.Length)];
+        }
+        audioSource.Play();
     }
 }
